b8bd334947f763717108cb175a00fd40bb600595
dylanabke <dja5837@psu.edu>
2025-02-11 13:17:09 -0500

    I added fancy

--- src/my-card.js
+++ src/my-card.js
@@ -15,23 +15,90 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQld3siqapzGeWKJIQMQ0hIPhy19xztUTMfEg&s";
+    this.link = "Details";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        
+      }
+      :host([fancy]) .card {
+        background-color: lightblue;
+      }
+
+
+      div {
+        background-color: gray;
+        border: 2px solid black;
+        padding: 10px;
+        margin: 10px;
+        width: 350px;
+        height: 500px;
+      }
+
+
+      h1 {
+        color: navy;
+        text-align: center;
+        font-size: 2em;
+        padding: 0;
+        margin:0;
+      }
+      img {
+        width: 80%;
+        height: auto;
+        align: center;
+        padding: 35px;
+      }
+      p {
+        font-size: 1.5em;
+        padding: 10px;
+        margin: 0;
+        text-align: center;
+      }
+      button {
+        background-color: white;
+        color: black;
+        font-size: 1.5em;
+        padding: 10px;
+        margin: 10px;
+        width: 50%;
+        border: none;
+        border-radius: 5px;
+        text-align: center;
+        justify-content: center;
       }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div>
+      <h1>${this.title}</h1>
+      <img src="${this.image}" alt ="${this.title}"/>
+      <a href=${this.link} target="_blank">
+        <button class="btn"><em>Details</em></button>
+        </a>
+      <details ?open="${this.fancy}">
+        <summary>Description</summary>
+        <div>
+          <slot>${this.description}</slot>
+        </div>
+      </details>
+      
+    </div>`;
   }
 
   static get properties() {
     return {
+      fancy: { type: Boolean, reflect: true },
       title: { type: String },
+      image: { type: String },
+      link: { type: String }
     };
   }
 }
